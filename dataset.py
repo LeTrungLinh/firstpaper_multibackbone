3ed7658f4e7e0129d5cd9c5f489533f482be0fcc
letrunglinha1 <letrunglinha1@gmail.com>
2023-09-05 16:18:05 +0700

    "first commit"

---
+++ dataset.py
@@ -0,0 +1,117 @@
+import os
+
+import cv2
+import numpy as np
+import torch
+import torch.utils.data
+from PIL import Image
+
+class Dataset(torch.utils.data.Dataset):
+    def __init__(self, img_ids, img_dir, mask_dir, img_ext, mask_ext, num_classes, transform=None):
+        """
+        Args:
+            img_ids (list): Image ids.
+            img_dir: Image file directory.
+            mask_dir: Mask file directory.
+            img_ext (str): Image file extension.
+            mask_ext (str): Mask file extension.
+            num_classes (int): Number of classes.
+            transform (Compose, optional): Compose transforms of albumentations. Defaults to None.
+        
+        Note:
+            Make sure to put the files as the following structure:
+            <dataset name>
+            ├── images
+            |   ├── 0a7e06.jpg
+            │   ├── 0aab0a.jpg
+            │   ├── 0b1761.jpg
+            │   ├── ...
+            |
+            └── masks
+                ├── 0
+                |   ├── 0a7e06.png
+                |   ├── 0aab0a.png
+                |   ├── 0b1761.png
+                |   ├── ...
+                |
+                ├── 1
+                |   ├── 0a7e06.png
+                |   ├── 0aab0a.png
+                |   ├── 0b1761.png
+                |   ├── ...
+                ...
+        """
+        self.img_ids = img_ids
+        self.img_dir = img_dir
+        self.mask_dir = mask_dir
+        self.img_ext = img_ext
+        # self.mask_ext = mask_ext
+        self.mask_ext = ".png"
+        # self.num_classes = num_classes
+        self.num_classes = 1
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.img_ids)
+
+    def __getitem__(self, idx):
+        img_id = self.img_ids[idx]
+        
+        img = cv2.imread(os.path.join(self.img_dir, img_id + self.img_ext))
+
+        mask = []
+        for i in range(self.num_classes):
+            mask.append(cv2.imread(os.path.join(self.mask_dir, str(i),
+                        img_id +"_segmentation"+ self.mask_ext), cv2.IMREAD_GRAYSCALE)[..., None])
+        mask = np.dstack(mask)
+
+        if self.transform is not None:
+            augmented = self.transform(image=img, mask=mask)
+            img = augmented['image']
+            mask = augmented['mask']
+        
+        img = img.astype('float32') / 255
+        img = img.transpose(2, 0, 1)
+        mask = mask.astype('float32') / 255
+        mask = mask.transpose(2, 0, 1)
+        
+        return img, mask, {'img_id': img_id}
+
+
+
+class NPY_datasets(Dataset):
+    def __init__(self, path_Data, config, train=True):
+        super(NPY_datasets, self)
+        if train:
+            images_list = os.listdir(path_Data+'train/images/')
+            masks_list = os.listdir(path_Data+'train/masks/')
+            images_list = sorted(images_list)
+            masks_list = sorted(masks_list)
+            self.data = []
+            for i in range(len(images_list)):
+                img_path = path_Data+'train/images/' + images_list[i]
+                mask_path = path_Data+'train/masks/' + masks_list[i]
+                self.data.append([img_path, mask_path])
+            self.transformer = config.train_transformer
+        else:
+            images_list = os.listdir(path_Data+'val/images/')
+            masks_list = os.listdir(path_Data+'val/masks/')
+            images_list = sorted(images_list)
+            masks_list = sorted(masks_list)
+            self.data = []
+            for i in range(len(images_list)):
+                img_path = path_Data+'val/images/' + images_list[i]
+                mask_path = path_Data+'val/masks/' + masks_list[i]
+                self.data.append([img_path, mask_path])
+            self.transformer = config.test_transformer
+        
+    def __getitem__(self, indx):
+        img_path, msk_path = self.data[indx]
+        img = np.array(Image.open(img_path).convert('RGB'))
+        msk = np.expand_dims(np.array(Image.open(msk_path).convert('L')), axis=2) / 255
+        img, msk = self.transformer((img, msk))
+        return img, msk, {'img_id': 0}
+
+    def __len__(self):
+        return len(self.data)
+        
